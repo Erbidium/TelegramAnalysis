4881a99cfe9f6f14c4689016ba2cf0416e332f50
Oleksandr <OlexandrTanasienko@gmail.com>
2024-04-03 19:58:44 +0300

    Add DAL entities

---
+++ ParsingProject/ParsingProject/ParsingProject.DAL/Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace ParsingProject.DAL.Entities;
+
+public class Comment
+{
+    public long Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    
+    public DateTime CreatedAt { get; set; }
+}
---
+++ ParsingProject/ParsingProject/ParsingProject.DAL/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace ParsingProject.DAL.Entities;
+
+public class Post
+{
+    public long Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public int ViewsCount { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public ICollection<Comment> Comments { get; set; }
+    //public List<int> Reactions { get; set; }
+}
