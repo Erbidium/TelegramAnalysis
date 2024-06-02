869ee331289d532cbd5529fa363aa99057af9e75
Oleksandr <OlexandrTanasienko@gmail.com>
2024-06-02 16:11:36 +0300

    Ignore warnings in environment file

--- FrontendProject/src/environments/environment.prod.ts
+++ FrontendProject/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
-// @ts-ignore
 export const environment = {
   production: true,
+  //@ts-ignore
   coreUrl: window["env"].coreUrl || 'https://localhost:44357',
+  //@ts-ignore
   analysisUrl: window["env"].analysisUrl || 'http://127.0.0.1:5000/'
 };
