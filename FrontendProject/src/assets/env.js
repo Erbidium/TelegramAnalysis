52887fb5879b415c49835c8e2df6b97f6655bacf
Oleksandr <OlexandrTanasienko@gmail.com>
2024-06-02 15:49:36 +0300

    Configure frontend environment variables setup

---
+++ FrontendProject/src/assets/env.js
@@ -0,0 +1,5 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+    window['env']['coreUrl'] = 'https://localhost:44357';
+    window['env']['analysisUrl'] = 'http://127.0.0.1:5000/';
+})(this);
---
+++ FrontendProject/src/assets/env.sample.js
@@ -0,0 +1,5 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+    window['env']['coreUrl'] = '${BACKEND_BASE_URL}';
+    window['env']['analysisUrl'] = '${ANALYSIS_BASE_URL}';
+})(this);
